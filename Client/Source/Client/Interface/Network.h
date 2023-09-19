7a47899c6e2cce9258a2dde1483feac2e77bc058
gws1017 <gws1017@naver.com>
2023-09-20 01:03:45 +0900

    네트워크 인터페이스 추가
    - Network.h (서버가 관리하는객체에 붙여줄 인터페이스, INetwork에 순수가상함수 작성)
    - BaseCharacter.h (서버관련 변수 추가시 이곳에)

---
+++ Client/Source/Client/Interface/Network.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Network.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UNetwork : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class CLIENT_API INetwork
+{
+	GENERATED_BODY()
+
+public:
+
+	//Server code
+	
+	//virtual bool ConnectServer() = 0;
+	//virtual void RecvPacket() = 0;
+};
