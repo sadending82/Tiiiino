d21ace1cc3cb3e9f971c239518d946f38d040c2d
gws1017 <gws1017@naver.com>
2023-09-19 21:38:17 +0900

    UHelpers 헤더, 언리얼 헤더 경로 설정
    - GetAsset : 에셋(메시,블루프린트 등)을 C++에서 등록하는데 사용하는 함수
    - GetAssetDynamic : 위와 동일한 기능인데 런타임에 변경할 경우 사용
    - CreateComponent: 컴포넌트(메시 컴포넌트, 카메라 컴포넌트 등) 부착하는 함수
    - GetClass: 에디터에있는 블루프린트 클래스 변수에 등록할때 주로사용
    - GetClassDynamic: 위기능과 동일하나 런타임시 사용
    
    - Client.Builds.cs 추가된 코드 => 상대경로도 인식가능하도록

---
+++ Client/Source/Client/Utilities/UHelpers.h
@@ -0,0 +1,71 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "UObject/ConstructorHelpers.h"
+
+class UE_ARPG_API UHelpers
+{
+public:
+
+	//에셋 파일을 C++에서 등록할때 사용
+	template<typename T>
+	static void GetAsset(T** OutObject, FString InPath)
+	{
+		ConstructorHelpers::FObjectFinder<T> asset(*InPath);
+		verifyf(asset.Succeeded(), L"asset.Succeeded()");
+		*OutObject = asset.Object;
+	}
+
+	//위와 기능은 동일, 게임 중에 에셋 변경시에 사용
+	template<typename T>
+	static void GetAssetDynamic(T** OutObject, FString InPath)
+	{
+		T* obj = Cast<T>(StaticLoadObject(T::StaticClass(), NULL, *InPath));
+		verifyf(!!obj, L"!!obj");
+		*OutObject = obj;
+	}
+
+	template<typename T>
+	static void CreateComponent(AActor* InActor, T** InComponent,
+		FName InName, USceneComponent* InParent = NULL)
+	{
+		*InComponent = InActor->CreateDefaultSubobject<T>(InName);
+
+		if (!!InParent)
+		{
+			(*InComponent)->SetupAttachment(InParent);
+			return;
+		}
+
+		InActor->SetRootComponent((*InComponent));
+	}
+
+	template<typename T>
+	static void GetClass(TSubclassOf<T>* OutClass, FString InPath)
+	{
+		ConstructorHelpers::FClassFinder<T> asset(*InPath);
+
+		verifyf(asset.Succeeded(), L"asset.Succeeded()");
+
+		*OutClass = asset.Class;
+	}
+
+	template<typename T>
+	static void GetClassDynamic(TSubclassOf<T>* OutClass, FString InPath)
+	{
+		TSubclassOf<T> cclass = StaticLoadClass(T::StaticClass(), NULL, *InPath);
+
+		verifyf(!!cclass, L"!! class");
+
+		*OutClass = cclass;
+	}
+
+	template<typename T>
+	static void FindActors(class UWorld* InWorld, TArray<T*>& OutActors)
+	{
+		TArray<AActor*> actors;
+		UGameplayStatics::GetAllActorsOfClass(InWorld, T::StaticClass(), actors);
+
+		for (AActor* actor : actors)
+			OutActors.Add(Cast<T>(actor));
+	}
+};
