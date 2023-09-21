c0692c5c0d1496288f7515cdcb3b8d23d7a79f9d
sadending82 <sadending82@gmail.com>
2023-09-21 15:36:19 +0900

    회전하는 봉을 추가
    - 일단은 서버 동기화는 생각하지 않고 일단은 AddLocalRotation으로 구현하였으나, 서버 동기화를 하는 경우에는 SetRotation으로 직접 값을 계산하여 부여해야 할 것으로 보입니다.
    - 해당 C++ 클래스의 stat 항목에서 탄성력과 회전 속도를 제어할 수 있도록 구현해 두었습니다.

---
+++ Client/Source/Client/Actor/RotatingPole.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RotatingPole.generated.h"
+
+UCLASS()
+class CLIENT_API ARotatingPole : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARotatingPole();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation,
+		FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere) UStaticMeshComponent* Mesh;
+
+private:
+	UPROPERTY(EditAnywhere, Category = Stat, Meta = (AllowPrivateAccess = true)) float RotateSpeed = 30.0f;
+	UPROPERTY(EditAnywhere, Category = Stat, Meta = (AllowPrivateAccess = true)) float ElasticForce = 500.0f;
+};
