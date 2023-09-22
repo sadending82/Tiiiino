882d9d56fca1fe15386cc621bda4d9d9341bb520
gws1017 <gws1017@naver.com>
2023-09-22 23:08:14 +0900

    레벨 C++ 코드 추가
    - CustomBaseLevel에 코드 작성시 모든 레벨에 해당 코드가 적용됩니다.
    - 로비레벨, 스테이지1(인게임레벨), 테스트레벨(Testmap) C++코드를 추가했습니다. 각레벨별로 다르게 적용할시 이곳에 작성하면됩니다.

---
+++ Client/Source/Client/Actor/Level/CustomLevel_Test.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actor/Level/CustomBaseLevel.h"
+#include "CustomLevel_Test.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIENT_API ACustomLevel_Test : public ACustomBaseLevel
+{
+	GENERATED_BODY()
+	
+};
