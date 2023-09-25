fbd69e4425a7581cff5699fc23ee2b9b21375398
boy0501 <boy0501@naver.com>
2023-09-26 02:49:45 +0900

    버그 수정 sleepEx

--- Client/Source/Client/Actor/Character/TinoCharacter.cpp
+++ Client/Source/Client/Actor/Character/TinoCharacter.cpp
@@ -65,9 +65,9 @@ void ATinoCharacter::Tick(float DeltaTime)
 
 		float PitchClamp = FMath::ClampAngle(Rotation.Pitch, -45.f, 30.f);
 		FRotator RotationControl(PitchClamp, Rotation.Yaw, Rotation.Roll);
-
+		SleepEx(0, true);
 		if (GetController()->IsPlayerController() && Network::GetNetwork()->bIsConnected) {
-			SleepEx(0, true);
+			
 			auto pos = GetTransform().GetLocation();
 			auto rot = GetTransform().GetRotation();
 
