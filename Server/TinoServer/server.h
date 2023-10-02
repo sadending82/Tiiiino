d2ed9cc02688bb1e27f172ec3e0a189d256344da
SeongJae Han <anis15740@gmail.com>
2023-10-02 18:35:22 +0900

    Update server.h

--- Server/TinoServer/server.h
+++ Server/TinoServer/server.h
@@ -16,6 +16,10 @@ public:
 	void DoWorker();
 	void Init();
 	void ProcessEvent(unsigned char* cmessage);
+
+	void CheckDuplicateLogin(int uid);
+
+	void SendDiconnectPacketToGameServer(int key, int uid, int roomID);
 public:
 	array <Session, MAX_USER> mClients;
 	array <Session, MAXGAMESERVER> mServers;
