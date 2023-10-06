92cd79d55b60f5d456200e69f2c187976d10ef4a
wjdrmrgns <rmrgns@naver.com>
2023-10-06 21:32:43 +0900

    Update FinishGameUI
    1. 게임이 끝났을 때 게임결과창 출력
    2. 임시로 z키를 눌렀을 때 결과창 내용이 바뀌도록 설정
    3. 결과창의 변수들은 c++에서 수정가능

--- Client/Source/Client/MenuUI/FinishGameUIWidget.cpp
+++ Client/Source/Client/MenuUI/FinishGameUIWidget.cpp
@@ -1,7 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "MenuUI/FinishGameUIWidget.h"
+#include "Components/EditableText.h"
 
 void UFinishGameUIWidget::NativePreConstruct()
 {
@@ -13,21 +14,35 @@ void UFinishGameUIWidget::NativeDestruct()
 
 void UFinishGameUIWidget::EnterLobby()
 {
+	// EnterLobbyButton Onclicked
+	// 로비로 돌아가기
 
 }
 
-void UFinishGameUIWidget::SetCheckResult(bool bcheckresult)
+void UFinishGameUIWidget::ShowResult()
 {
-	bCheckResult = bcheckresult;
-}
+	// 맵 이름 입력
+	CurrentStage = TEXT("맵 이름");	// 맵 이름 입력
+	FText CurrentStageText = FText::FromString(CurrentStage);
+	EditSTAGEText->SetText(CurrentStageText);
 
-void UFinishGameUIWidget::SuccessGame()
-{
 
-}
+	// 이 판 등수 입력
+	CurrentRank = 1;	// 등수 입력
+	FText CurrentRankText = FText::AsNumber(CurrentRank);
+	EditRANKText->SetText(CurrentRankText);
 
-void UFinishGameUIWidget::FailedGame()
-{
+
+	// 획득한 학점 입력
+	GainGrade = 10;	// 등수 입력
+	FText GainGradeText = FText::AsNumber(GainGrade);
+	EditGradeText->SetText(GainGradeText);
+
+
+	// 획득한 포인트 입력
+	GainPoint = 100;	// 등수 입력
+	FText GainPointText = FText::AsNumber(GainPoint);
+	EditPointText->SetText(GainPointText);
 
 }
 
