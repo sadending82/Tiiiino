0574a18fe075b3785fb1040f91d109cf0c1931c4
boy0501 <boy0501@naver.com>
2023-10-27 13:35:24 +0900

    db이슈해결 ( 노 로그인 디비)

--- Server/DBServer/DBServer/cSocket.cpp
+++ Server/DBServer/DBServer/cSocket.cpp
@@ -314,7 +314,7 @@ void Socket::ProcessPacket_Login(int key, unsigned char* buf)
     int uid = SetUIDForTest();
 
     SendLoginOK(key, uid, p->id
-        , 3.0, 100000, 1, 0, p->userKey, 0);
+        , 3.0, 100000, 1, 0, 0, p->userKey);
 }
 
 void Socket::ProcessPacket_Logout(unsigned char* buf)
